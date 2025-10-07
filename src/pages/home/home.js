ed1ed59a40907a92b559ff3ccdfe29f0e05f62bf
Shyam Kumar Limbu <shyam@Shyams-MacBook-Pro.local>
2025-10-07 17:17:14 +0800

    prototype02

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,11 +3,11 @@ import './home.css'
 const Home = () => {
     return(
         <Link to='/blog'>
-            <img src='/wallpaper.png' alt='wallpaper' className='wallpaper'/>
-            <img src='/logo.png' alt='logo' className='icon3' />
+            <img src='/MUSIC-PROJECT/wallpaper.png' alt='wallpaper' className='wallpaper'/>
+            <img src='/MUSIC-PROJECT/logo.png' alt='logo' className='icon3' />
             <div className='bottom-logo'>
-                <img src='/icon2.png' alt='logo' className='icon2' />
-                <img src='/icon1.png' alt='logo' className='icon1' />
+                <img src='/MUSIC-PROJECT/icon2.png' alt='logo' className='icon2' />
+                <img src='/MUSIC-PROJECT/icon1.png' alt='logo' className='icon1' />
             </div>
         </Link>
     )
